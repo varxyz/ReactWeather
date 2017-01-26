19ffb345df748d8de16efab4f54a820271f385d3
Sergiu Moraru <tyler@100.0.168.192.in-addr.arpa>
2017-01-26 08:49:23 +0200

    prep repo for heroku

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if (req.header['x-forwarder-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(port, () => console.log('Server running on port ' + port));
