4601f4e8d83becfafb504c5be71ba66e670890f9
Sergiu Moraru <tyler@100.0.168.192.in-addr.arpa>
2017-01-26 15:08:20 +0200

    added example locations

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //   render: function() {
@@ -11,8 +12,16 @@ var React = require('react');
 var Examples = (props) => {
   return (
       <div>
-        <h2>Examples</h2>
-        <p>Welcome to Examples page!</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are few examples to try out!</p>
+        <ol>
+          <li>
+            <Link to='/?location=Miami'>Miami, FL</Link>
+          </li>
+          <li>
+            <Link to='/?location=Rio'>Rio, Brazil</Link>
+          </li>
+        </ol>
       </div>
   );
 };
